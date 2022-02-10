6ff1311a5b2fdd94b94db4a401cab1e0da128a62
Adilet Nasirov <adilet.nasirov@yahoo.com>
2022-02-10 12:36:14 -0800

    added inputHandler, filter & onEnter funcs, styled

--- src/App.js
+++ src/App.js
@@ -8,13 +8,44 @@ class App extends React.Component {
     super();
     this.state = {
       currVal: "",
+      filteredArr: [],
     };
   }
+
+  handleInput = (e) => {
+    let val = e.target.value;
+    // console.log(val);
+    if (val.length !== 0) {
+      const filtered = data.filter((film) =>
+        film.label.toLowerCase().includes(val.toLowerCase())
+      );
+      this.setState({ currVal: val, filteredArr: filtered });
+    } else {
+      this.setState({ filteredArr: [], currVal: "" });
+    }
+  };
+
+  fillInput = (e) => {
+    // console.log(e.target.innerText);
+    this.setState({ currVal: e.target.innerText });
+  };
+
+  handleEnter = (e) => {
+    if (e.keyCode === 13) {
+      this.setState({ currVal: this.state.filteredArr[0].label });
+    }
+  };
   render() {
-    console.log(data);
+    // console.log(this.state);
     return (
       <div className="App">
-        <Search />
+        <Search
+          handleInput={this.handleInput}
+          filteredData={this.state.filteredArr}
+          fillInput={this.fillInput}
+          currVal={this.state.currVal}
+          handleEnter={this.handleEnter}
+        />
       </div>
     );
   }
--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,16 +1,38 @@
-import React from "react";
-import TextField from "@mui/material/TextField";
-
-const Search = () => {
+import * as React from "react";
+import "./search.css";
+const Search = ({
+  handleInput,
+  filteredData,
+  fillInput,
+  currVal,
+  handleEnter,
+}) => {
   return (
-    <div>
-      <TextField id="outlined-search" label="Search field" type="search" />
-      <TextField
-        id="outlined-helperText"
-        label="Helper text"
-        defaultValue="Default Value"
-        helperText="Some important text"
+    <div className="container">
+      <input
+        type="text"
+        onKeyUp={handleEnter}
+        onChange={handleInput}
+        value={currVal}
       />
+      <div
+        className="output"
+        style={
+          filteredData.length > 0 ? { display: "block" } : { display: "none" }
+        }
+      >
+        {filteredData.length > 0 ? (
+          filteredData.map((item, index) => {
+            return (
+              <p className="movie" onClick={fillInput} key={index}>
+                {item.label}
+              </p>
+            );
+          })
+        ) : (
+          <></>
+        )}
+      </div>
     </div>
   );
 };
